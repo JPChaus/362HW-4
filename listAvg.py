d35825b3b1234e8a7127a4d8110ca2def836b928
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-05-03 18:01:43 -0400

    code for all three programs

---
+++ listAvg.py
@@ -0,0 +1,34 @@
+def main():
+    list_length = uInput("How big do you want the list? Positive integer value only: ", 1)
+    my_list = []
+    for i in range(0, list_length):
+        num = uInput("Enter a number to add to the list. Integer value only: ", 0)
+        my_list.append(num)
+    answer = calculateAvg(my_list)
+    print("Average of the list: ", answer)
+
+def uInput(x, y):
+    if(y == 0):
+        while True:
+            try:
+                z = int(input(x))
+                break
+            except ValueError:
+                print("Invalid input!")
+    elif(y == 1):
+        while True:
+            try:
+                z = int(input(x))
+                if(z < 1):
+                    raise ValueError
+                break
+            except ValueError:
+                print("Invalid input!")
+    return z
+
+def calculateAvg(x):
+    listAvg = sum(x)/len(x)
+    return listAvg
+
+if __name__ == '__main__':
+    main()
